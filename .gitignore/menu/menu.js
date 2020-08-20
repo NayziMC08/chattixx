2aea35a95f2c10c727f32609f75f18215943a513
NayziMC08 <69964235+NayziMC08@users.noreply.github.com>
2020-08-20 14:25:59 +0200

    Update menu.js

--- .gitignore/menu/menu.js
+++ .gitignore/menu/menu.js
@@ -1,4 +1,4 @@
 const Discord = require('discord.js');
 const client = new Discord.Client(); 
 
-client.login("NzQ1OTQ3Nzc2NzY4Mjc4NTc4.Xz5MAQ.rfZ2ZPfug2VFrwWHRQbCWrP8yEY")
+client.login("NzQ1OTQ3Nzc2NzY4Mjc4NTc4.Xz5MAQ.Nt6vQjICIhuhu3eKpBGK9v9H-CM")
